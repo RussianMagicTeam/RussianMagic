ff8d46c6d29850fa9feade1df96a1c920aedf2dc
Regela <eliktr@yandex.ru>
2020-10-05 19:31:45 +0300

    Initial commit

---
+++ src/main/java/ru/rikgela/russinamagic/ExampleMod.kt
@@ -0,0 +1,24 @@
+package ru.rikgela.russinamagic
+
+import net.minecraft.item.Item
+import net.minecraft.item.ItemGroup
+import net.minecraftforge.fml.RegistryObject
+import net.minecraftforge.fml.common.Mod
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext
+import net.minecraftforge.registries.DeferredRegister
+import net.minecraftforge.registries.ForgeRegistries
+
+
+object ModItems {
+    val ITEMS: DeferredRegister<Item> = DeferredRegister(ForgeRegistries.ITEMS, "russianmagic")
+    val STONE_STICK: RegistryObject<Item> = ITEMS.register<Item>("stone_magic_object") { Item(Item.Properties().group(ItemGroup.TOOLS)) }
+}
+// The value here should match an entry in the META-INF/mods.toml file
+@Mod("russianmagic")
+@Mod.EventBusSubscriber(bus = Mod.EventBusSubscriber.Bus.MOD)
+class ExampleMod {
+    init{
+        val bus = FMLJavaModLoadingContext.get().modEventBus
+        ModItems.ITEMS.register(bus)
+    }
+}
