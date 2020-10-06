3746bafec62ce9c19e0c8b31d30996ccc247c5b7
Regela <eliktr@yandex.ru>
2020-10-06 19:37:01 +0300

    Uncomment not worked code

--- src/main/kotlin/ru/rikgela/russianmagic/Player.kt
+++ src/main/kotlin/ru/rikgela/russianmagic/Player.kt
@@ -1,34 +1,33 @@
-/*package ru.rikgela.russianmagic
+package ru.rikgela.russianmagic
 
-//CapabilityManager.INSTANCE.register(IManaHandler.class, new Storage(), DefaultManaHandler.class);
+CapabilityManager.INSTANCE.register(IManaHandler.class, new Storage(), DefaultManaHandler.class);
 
-//@SubscribeEvent
-//class MyForgeEventHandler {
-//    @SubscribeEvent
-//    fun pickupItem(event: EntityItemPickupEvent?) {
-//        println("Item picked up!")
-//    }
-//}
+@SubscribeEvent
+class MyForgeEventHandler {
+    @SubscribeEvent
+    fun pickupItem(event: EntityItemPickupEvent?) {
+        println("Item picked up!")
+    }
+}
 
 
-//
-//class EventsHandler {
-//    @SubscribeEvent
-//    fun onJoin(e: EntityJoinWorldEvent) {
-//        if (e.entity is PlayerEntity) {
-//            val player: PlayerEntity = e.entity as PlayerEntity
-//            //val tmp = "Hello, " + player.getName() + "!"
-//            //ITextComponent text  = new TextComponentTranslation(tmp)
-//
-//        }
-//    }
-//
-//    @SubscribeEvent
-//    fun onDeath(e: LivingDeathEvent) {
-//        if (e.entity is PlayerEntity) {
-//            val player: PlayerEntity = e.entity as PlayerEntity
-//            if (player.getName().equals("_Ivasik_")) player.dropItem(ItemStack(Items.GOLDEN_APPLE, 1, 1), false)
-//        }
-//    }
-//}
-*/
+
+class EventsHandler {
+    @SubscribeEvent
+    fun onJoin(e: EntityJoinWorldEvent) {
+        if (e.entity is PlayerEntity) {
+            val player: PlayerEntity = e.entity as PlayerEntity
+            //val tmp = "Hello, " + player.getName() + "!"
+            //ITextComponent text  = new TextComponentTranslation(tmp)
+
+        }
+    }
+
+    @SubscribeEvent
+    fun onDeath(e: LivingDeathEvent) {
+        if (e.entity is PlayerEntity) {
+            val player: PlayerEntity = e.entity as PlayerEntity
+            if (player.getName().equals("_Ivasik_")) player.dropItem(ItemStack(Items.GOLDEN_APPLE, 1, 1), false)
+        }
+    }
+}
