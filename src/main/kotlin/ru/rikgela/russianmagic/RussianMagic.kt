f1adb26883b1cac751d4fbecdd498d19b1e67a87
Regela <eliktr@yandex.ru>
2020-10-06 22:19:39 +0300

    Some refactor

--- src/main/kotlin/ru/rikgela/russianmagic/RussianMagic.kt
+++ src/main/kotlin/ru/rikgela/russianmagic/RussianMagic.kt
@@ -1,8 +1,12 @@
 package ru.rikgela.russianmagic
 
 import net.minecraftforge.common.MinecraftForge
+import net.minecraftforge.common.capabilities.CapabilityManager
+import net.minecraftforge.eventbus.api.SubscribeEvent
 import net.minecraftforge.fml.common.Mod
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext
+import ru.rikgela.russianmagic.mana.*
+
 
 const val MOD_ID = "russianmagic"
 
@@ -14,5 +18,9 @@ class RussianMagic {
         Items.ITEMS.register(bus)
         Blocks.BLOCKS.register(bus)
         MinecraftForge.EVENT_BUS.register(MyForgeEventHandler())
+        MinecraftForge.EVENT_BUS.register(ManaCapabilityHandler())
+        MinecraftForge.EVENT_BUS.register(EventHandler())
+        CapabilityManager.INSTANCE.register(IMana::class.java, ManaStorage()) { Mana() }
+
     }
 }
---
+++ src/main/kotlin/ru/rikgela/russianmagic/mana/Mana.kt
@@ -0,0 +1,116 @@
+package ru.rikgela.russianmagic.mana
+
+import net.minecraft.entity.Entity
+import net.minecraft.entity.player.PlayerEntity
+import net.minecraft.nbt.INBT
+import net.minecraft.nbt.IntNBT
+import net.minecraft.util.Direction
+import net.minecraft.util.ResourceLocation
+import net.minecraft.util.text.StringTextComponent
+import net.minecraftforge.common.capabilities.Capability
+import net.minecraftforge.common.capabilities.Capability.IStorage
+import net.minecraftforge.common.capabilities.CapabilityInject
+import net.minecraftforge.common.capabilities.ICapabilitySerializable
+import net.minecraftforge.common.util.LazyOptional
+import net.minecraftforge.common.util.NonNullSupplier
+import net.minecraftforge.event.AttachCapabilitiesEvent
+import net.minecraftforge.event.entity.living.LivingFallEvent
+import net.minecraftforge.event.entity.player.PlayerEvent.PlayerLoggedInEvent
+import net.minecraftforge.event.entity.player.PlayerSleepInBedEvent
+import net.minecraftforge.eventbus.api.SubscribeEvent
+import ru.rikgela.russianmagic.MOD_ID
+
+
+interface IMana {
+    fun consume(points: Int)
+    fun fill(points: Int)
+    fun set(points: Int)
+    val mana: Int
+}
+
+class Mana : IMana {
+    override var mana = 250
+        private set
+
+    override fun consume(points: Int) {
+        mana -= points
+        if (mana < 0) mana = 0
+    }
+
+    override fun fill(points: Int) {
+        mana += points
+    }
+
+    override fun set(points: Int) {
+        mana = points
+    }
+
+}
+
+class ManaStorage : IStorage<IMana> {
+    override fun writeNBT(capability: Capability<IMana>, instance: IMana, side: Direction): INBT {
+        return IntNBT.valueOf(instance.mana)
+    }
+
+    override fun readNBT(capability: Capability<IMana>, instance: IMana, side: Direction, nbt: INBT) {
+        instance.set((nbt as IntNBT).int)
+    }
+}
+
+class ManaProvider : ICapabilitySerializable<INBT>
+{
+    companion object{
+        @CapabilityInject(IMana::class)
+        val MANA_CAP: Capability<IMana>? = null
+    }
+    val instance: IMana? = MANA_CAP?.defaultInstance
+
+    @Override
+    fun hasCapability(capability: Capability<Any> , facing: Direction ): Boolean
+    {
+        return capability == MANA_CAP;
+    }
+
+    override fun <T : Any?> getCapability(cap: Capability<T>, side: Direction?): LazyOptional<T> {
+         return if(cap == MANA_CAP) LazyOptional.of(instance as NonNullSupplier<T>) else LazyOptional.empty()
+    }
+
+    override fun deserializeNBT(nbt: INBT?) {
+        MANA_CAP?.storage?.readNBT(MANA_CAP, this.instance, null, nbt)
+    }
+
+    override fun serializeNBT(): INBT {
+        return MANA_CAP?.storage?.writeNBT(MANA_CAP, this.instance, null) ?: IntNBT.valueOf(0)
+    }
+}
+
+class ManaCapabilityHandler {
+    @SubscribeEvent
+    fun attachCapability(event: AttachCapabilitiesEvent<Entity>) {
+        event.addCapability(MANA_CAP, ManaProvider())
+    }
+
+    companion object {
+        val MANA_CAP = ResourceLocation(MOD_ID, "mana")
+    }
+}
+
+class EventHandler {
+    @SubscribeEvent
+    fun onPlayerLogsIn(event: PlayerLoggedInEvent) {
+        val player: PlayerEntity = event.player
+        val mana: IMana = player.getCapability(ManaProvider.MANA_CAP!!, null).orElse(Mana())
+        val message = String.format("Hello there, you have §7%d§r mana left.", mana.mana)
+        player.sendMessage(StringTextComponent(message))
+    }
+
+    @SubscribeEvent
+    fun onPlayerSleep(event: PlayerSleepInBedEvent) {
+        val player: PlayerEntity = event.player
+//        if (player.worldObj.isRemote) return
+        val mana: IMana = player.getCapability(ManaProvider.MANA_CAP!!, null).orElse(Mana())
+        mana.fill(50)
+        val message = String.format("You refreshed yourself in the bed. You received 50 mana, you have §7%d§r mana left.", mana.mana)
+        player.sendMessage(StringTextComponent(message))
+    }
+}
