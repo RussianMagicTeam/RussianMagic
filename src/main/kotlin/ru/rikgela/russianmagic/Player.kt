5b6f151b39154b3998480decbd9f4c6c5ad24a39
Regela <eliktr@yandex.ru>
2020-10-06 19:32:27 +0300

    Repair master

--- src/main/kotlin/ru/rikgela/russianmagic/Player.kt
+++ src/main/kotlin/ru/rikgela/russianmagic/Player.kt
@@ -1,41 +1,34 @@
 /*package ru.rikgela.russianmagic
 
-import net.minecraft.entity.player.PlayerEntity
-import net.minecraft.item.ItemStack
-import net.minecraftforge.event.entity.EntityJoinWorldEvent
-import net.minecraftforge.event.entity.living.LivingDeathEvent
-import net.minecraftforge.eventbus.api.SubscribeEvent
-
-
 //CapabilityManager.INSTANCE.register(IManaHandler.class, new Storage(), DefaultManaHandler.class);
 
-/*@SubscribeEvent
-class MyForgeEventHandler {
-    @SubscribeEvent
-    fun pickupItem(event: EntityItemPickupEvent?) {
-        println("Item picked up!")
-    }
-}
+//@SubscribeEvent
+//class MyForgeEventHandler {
+//    @SubscribeEvent
+//    fun pickupItem(event: EntityItemPickupEvent?) {
+//        println("Item picked up!")
+//    }
+//}
 
- */
 
-class EventsHandler {
-    @SubscribeEvent
-    fun onJoin(e: EntityJoinWorldEvent) {
-        if (e.entity is PlayerEntity) {
-            val player: PlayerEntity = e.entity as PlayerEntity
-            //val tmp = "Hello, " + player.getName() + "!"
-            //ITextComponent text  = new TextComponentTranslation(tmp)
-
-        }
-    }
-
-    @SubscribeEvent
-    fun onDeath(e: LivingDeathEvent) {
-        if (e.entity is PlayerEntity) {
-            val player: PlayerEntity = e.entity as PlayerEntity
-            if (player.getName().equals("_Ivasik_")) player.dropItem(ItemStack(Items.GOLDEN_APPLE, 1, 1), false)
-        }
-    }
-}
+//
+//class EventsHandler {
+//    @SubscribeEvent
+//    fun onJoin(e: EntityJoinWorldEvent) {
+//        if (e.entity is PlayerEntity) {
+//            val player: PlayerEntity = e.entity as PlayerEntity
+//            //val tmp = "Hello, " + player.getName() + "!"
+//            //ITextComponent text  = new TextComponentTranslation(tmp)
+//
+//        }
+//    }
+//
+//    @SubscribeEvent
+//    fun onDeath(e: LivingDeathEvent) {
+//        if (e.entity is PlayerEntity) {
+//            val player: PlayerEntity = e.entity as PlayerEntity
+//            if (player.getName().equals("_Ivasik_")) player.dropItem(ItemStack(Items.GOLDEN_APPLE, 1, 1), false)
+//        }
+//    }
+//}
 */
