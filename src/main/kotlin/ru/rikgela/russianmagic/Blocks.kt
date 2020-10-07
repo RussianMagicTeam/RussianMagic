e8432d68a92807a31f22966e2f47eee4c9164ace
Strannick112 <43038491+Strannick112@users.noreply.github.com>
2020-10-07 14:59:34 +0300

    White jade block update

--- src/main/kotlin/ru/rikgela/russianmagic/Blocks.kt
+++ src/main/kotlin/ru/rikgela/russianmagic/Blocks.kt
@@ -23,5 +23,12 @@ object Blocks {
                 .hardnessAndResistance(3.0f, 3.0f)
                 .harvestTool(ToolType.PICKAXE))
     }
+    val WHITE_JADE_BLOCK: RegistryObject<Block> = BLOCKS.register<Block>("white_jade_block") {
+        Block(Block
+                .Properties
+                .create(Material.ROCK)
+                .hardnessAndResistance(3.0f, 3.0f)
+                .harvestTool(ToolType.PICKAXE))
+    }
 
 }
