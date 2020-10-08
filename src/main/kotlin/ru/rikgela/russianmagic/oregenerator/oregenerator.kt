5ecb3732b8e8a06c54cf3fdaa4b14ba795ebcefd
koliagas <koli97@bk.ru>
2020-10-08 15:59:10 +0300

    rework code for ore generator

--- src/main/kotlin/ru/rikgela/russianmagic/oregenerator/oregenerator.kt
+++ src/main/kotlin/ru/rikgela/russianmagic/oregenerator/oregenerator.kt
@@ -13,10 +13,22 @@ import net.minecraftforge.registries.ForgeRegistries
 object OreGeneration {
     fun setupOreGeneration() {
             for (biome in ForgeRegistries.BIOMES) {
-                biome.addFeature(GenerationStage.Decoration.UNDERGROUND_ORES, Feature.ORE.withConfiguration(OreFeatureConfig(OreFeatureConfig.FillerBlockType.NATURAL_STONE, Blocks.AQUAMARINE_BLOCK_ORE.get().getDefaultState(),10)).withPlacement(Placement.COUNT_RANGE.configure(CountRangeConfig(20, 0, 100, 256))))
-                biome.addFeature(GenerationStage.Decoration.UNDERGROUND_ORES, Feature.ORE.withConfiguration(OreFeatureConfig(OreFeatureConfig.FillerBlockType.NATURAL_STONE, Blocks.RHINESTONE_BLOCK_ORE.get().getDefaultState(), 10)).withPlacement(Placement.COUNT_RANGE.configure(CountRangeConfig(20, 0, 100, 256))))
-                biome.addFeature(GenerationStage.Decoration.UNDERGROUND_ORES, Feature.ORE.withConfiguration(OreFeatureConfig(OreFeatureConfig.FillerBlockType.NATURAL_STONE, Blocks.MARBLE_BLOCK.get().getDefaultState(), 10)).withPlacement(Placement.COUNT_RANGE.configure(CountRangeConfig(20, 0, 100, 256))))
-                biome.addFeature(GenerationStage.Decoration.UNDERGROUND_ORES, Feature.ORE.withConfiguration(OreFeatureConfig(OreFeatureConfig.FillerBlockType.NATURAL_STONE, Blocks.WHITE_JADE_BLOCK.get().getDefaultState(), 10)).withPlacement(Placement.COUNT_RANGE.configure(CountRangeConfig(20, 0, 100, 256))))
+                val s = OreFeatureConfig.FillerBlockType.NATURAL_STONE
+                biome.addFeature(GenerationStage.Decoration.UNDERGROUND_ORES, Feature.ORE.withConfiguration(OreFeatureConfig
+                (s , Blocks.AQUAMARINE_BLOCK_ORE.get().getDefaultState(),10)).
+                withPlacement(Placement.COUNT_RANGE.configure(CountRangeConfig(20, 0, 100, 256))))
+
+                biome.addFeature(GenerationStage.Decoration.UNDERGROUND_ORES, Feature.ORE.withConfiguration(OreFeatureConfig
+                (s , Blocks.RHINESTONE_BLOCK_ORE.get().getDefaultState(), 10)).
+                withPlacement(Placement.COUNT_RANGE.configure(CountRangeConfig(20, 0, 100, 256))))
+
+                biome.addFeature(GenerationStage.Decoration.UNDERGROUND_ORES, Feature.ORE.withConfiguration(OreFeatureConfig
+                ( s , Blocks.MARBLE_BLOCK.get().getDefaultState(), 10)).
+                withPlacement(Placement.COUNT_RANGE.configure(CountRangeConfig(20, 0, 100, 256))))
+
+                biome.addFeature(GenerationStage.Decoration.UNDERGROUND_ORES, Feature.ORE.withConfiguration(OreFeatureConfig
+                (s, Blocks.WHITE_JADE_BLOCK.get().getDefaultState(), 10)).
+                withPlacement(Placement.COUNT_RANGE.configure(CountRangeConfig(20, 0, 100, 256))))
         }
     }
 }
