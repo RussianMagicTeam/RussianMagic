ef54e6a2627de3f601895fceda61a100e9ad8231
Strannick112 <43038491+Strannick112@users.noreply.github.com>
2020-10-17 19:16:41 +0300

    rename as rm_furnace

--- src/main/kotlin/ru/rikgela/russianmagic/RussianMagic.kt
+++ src/main/kotlin/ru/rikgela/russianmagic/RussianMagic.kt
@@ -14,6 +14,7 @@ import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext
 import ru.rikgela.russianmagic.client.gui.RMFurnaceScreen
+import ru.rikgela.russianmagic.common.RMNetworkChannel
 import ru.rikgela.russianmagic.container.RMFurnaceContainer
 import ru.rikgela.russianmagic.init.*
 import ru.rikgela.russianmagic.mana.*
