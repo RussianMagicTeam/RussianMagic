65ae223abee8a4eb3db84974c4bd4279f085955a
Strannick112 <43038491+Strannick112@users.noreply.github.com>
2021-08-10 14:50:14 +0300

    xp hud is added

--- src/main/kotlin/ru/rikgela/russianmagic/client/gui/GuiXpHUD.kt
+++ src/main/kotlin/ru/rikgela/russianmagic/client/gui/GuiXpHUD.kt
@@ -12,10 +12,10 @@ import java.lang.Float.min
 
 @OnlyIn(Dist.CLIENT)
 class GuiXpHUD(
-        private val height: Int = 300,
-        private val width: Int = 200,
-        private val xPos: Int = 300,
-        private val yPos: Int = 405,
+        private val height: Int = 10,
+        private val width: Int = 100,
+        private val xPos: Int = 1,
+        private val yPos: Int = 89,
 ) : AbstractGui() {
 
     fun drawHUD() {
@@ -36,8 +36,10 @@ class GuiXpHUD(
         val x = minecraft.mainWindow.scaledWidth * xPos / 100
         val y = minecraft.mainWindow.scaledHeight * yPos / 100
         val mana = PlayerMana.fromPlayer(minecraft.player as PlayerEntity)
+
         Minecraft.getInstance().fontRenderer.drawString(
                 mana.lvl.toString() + ": " + mana.lvlExp.toString(), x.toFloat() + 3, y.toFloat() - 8, TextFormatting.WHITE.color!!)
+
     }
 
     companion object {
