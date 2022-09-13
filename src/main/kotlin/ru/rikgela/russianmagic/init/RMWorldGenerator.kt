2f18d6bb8c28131fb7de4eb1b7462ffa9a4eb44c
vladmialkin <myalkin.vlad@yandex.ru>
2022-09-13 22:05:50 +0300

    Увеличение генерации всей руды; Изменение генерации аквамарина только в биоме океана и рек.

--- src/main/kotlin/ru/rikgela/russianmagic/init/RMWorldGenerator.kt
+++ src/main/kotlin/ru/rikgela/russianmagic/init/RMWorldGenerator.kt
@@ -14,24 +14,28 @@ import ru.rikgela.russianmagic.objects.structures.EbonyTree
 
 
 object OreGeneration {
-    fun setupOreGeneration() {
+        fun setupOreGeneration() {
             for (biome in ForgeRegistries.BIOMES) {
                 val naturalStone = OreFeatureConfig.FillerBlockType.NATURAL_STONE
-                biome.addFeature(GenerationStage.Decoration.UNDERGROUND_ORES, Feature.ORE.withConfiguration(OreFeatureConfig
-                (naturalStone, RMBlocks.AQUAMARINE_BLOCK_ORE.get().defaultState, 10))
-                        .withPlacement(Placement.COUNT_RANGE.configure(CountRangeConfig(20, 0, 100, 256))))
+                Biomes.OCEAN.addFeature(GenerationStage.Decoration.UNDERGROUND_ORES, Feature.ORE.withConfiguration(OreFeatureConfig
+                (naturalStone, RMBlocks.AQUAMARINE_BLOCK_ORE.get().defaultState, 50))
+                        .withPlacement(Placement.COUNT_RANGE.configure(CountRangeConfig(20, 0, 10, 256))))
+
+                Biomes.RIVER.addFeature(GenerationStage.Decoration.UNDERGROUND_ORES, Feature.ORE.withConfiguration(OreFeatureConfig
+                (naturalStone, RMBlocks.AQUAMARINE_BLOCK_ORE.get().defaultState, 50))
+                        .withPlacement(Placement.COUNT_RANGE.configure(CountRangeConfig(20, 0, 10, 256))))
 
                 biome.addFeature(GenerationStage.Decoration.UNDERGROUND_ORES, Feature.ORE.withConfiguration(OreFeatureConfig
-                (naturalStone, RMBlocks.RHINESTONE_BLOCK_ORE.get().defaultState, 10))
-                        .withPlacement(Placement.COUNT_RANGE.configure(CountRangeConfig(20, 0, 100, 256))))
+                (naturalStone, RMBlocks.RHINESTONE_BLOCK_ORE.get().defaultState, 50))
+                        .withPlacement(Placement.COUNT_RANGE.configure(CountRangeConfig(20, 0, 10, 256))))
 
                 biome.addFeature(GenerationStage.Decoration.UNDERGROUND_ORES, Feature.ORE.withConfiguration(OreFeatureConfig
-                (naturalStone, RMBlocks.MARBLE_BLOCK.get().defaultState, 10))
-                        .withPlacement(Placement.COUNT_RANGE.configure(CountRangeConfig(20, 0, 100, 256))))
+                (naturalStone, RMBlocks.MARBLE_BLOCK.get().defaultState, 50))
+                        .withPlacement(Placement.COUNT_RANGE.configure(CountRangeConfig(20, 0, 10, 256))))
 
                 biome.addFeature(GenerationStage.Decoration.UNDERGROUND_ORES, Feature.ORE.withConfiguration(OreFeatureConfig
-                (naturalStone, RMBlocks.WHITE_JADE_BLOCK.get().defaultState, 10))
-                        .withPlacement(Placement.COUNT_RANGE.configure(CountRangeConfig(20, 0, 100, 256))))
+                (naturalStone, RMBlocks.WHITE_JADE_BLOCK.get().defaultState, 50))
+                        .withPlacement(Placement.COUNT_RANGE.configure(CountRangeConfig(20, 0, 10, 256))))
             }
         Biomes.JUNGLE.addFeature(GenerationStage.Decoration.VEGETAL_DECORATION, Feature.NORMAL_TREE.withConfiguration(EbonyTree.TREE_CONFIG).withPlacement(Placement.COUNT_HEIGHTMAP_32.configure(FrequencyConfig(1))))
         Biomes.JUNGLE_EDGE.addFeature(GenerationStage.Decoration.VEGETAL_DECORATION, Feature.NORMAL_TREE.withConfiguration(EbonyTree.TREE_CONFIG).withPlacement(Placement.COUNT_HEIGHTMAP_32.configure(FrequencyConfig(1))))
